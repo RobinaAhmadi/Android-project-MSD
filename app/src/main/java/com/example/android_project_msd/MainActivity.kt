5962a37229731897eecf7f804a25c434335b8000
NickBang <niban20@student.sdu.dk>
2025-11-05 10:59:56 +0100

    added gradle + user profile page

--- app/src/main/java/com/example/android_project_msd/MainActivity.kt
+++ app/src/main/java/com/example/android_project_msd/MainActivity.kt
@@ -7,22 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-// Importer din nye skærm
 import com.example.android_project_msd.profile.ProfileScreen
-// Du skal måske også importere dit tema
 import com.example.android_project_msd.ui.theme.AndroidprojectmsdTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Sæt dit tema
             AndroidprojectmsdTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Kald din nye ProfileScreen Composable her!
                     ProfileScreen()
                 }
             }
